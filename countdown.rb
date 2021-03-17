dcd3daf197f90e0001541653ca5cbcc6877cbed5
Craig <suesscraig112715@gmail.com>
2021-03-17 14:24:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,12 +5,16 @@ def countdown(integer)
   
   while integer >= counter
    puts "#{integer} SECOND(S)!";
-   integer -= 1
+   integer -= 1;
   end 
-  return "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!";
 end
+
+#while the integer given, is greater than the variable of counter, puts that out, subtract 1 from it, and repeat. once the seconds reach 1s, print code. 
 
 
 def countdown_with_sleep(integer)
-  sleep(integer)
+  sleep(integer);
 end 
+
+#slow ruby down, while impleminting the countdown method using the same integer passed in to the countdown method. 
