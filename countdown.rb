56a0dc85c2448a3f28c964580596c996be3c9430
Craig <suesscraig112715@gmail.com>
2021-03-17 14:18:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  counter = 1
+  
+  while integer > counter
+   puts "#{integer} SECOND(S)!";
+   integer -= 1
+  end 
+  return "HAPPY NEW YEAR!"
 end
